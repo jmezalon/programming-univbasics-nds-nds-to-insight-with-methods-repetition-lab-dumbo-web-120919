099704ef7d4af0b245fe09eac67b5ebf8923a44d
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-23 12:01:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,18 +45,15 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  # total = 0
+  total = 0
   index = 0 
-  # while index < source.length do
-    
-    total = directors_totals.select { |v| v.worldwide_gross + index } 
-    
-    # index += 1
-  # end 
-  
-  
-  
-  puts total
+  while index < source.length do
+    total += directors_totals(list_of_directors(index))
+
+    index += 1
+  end 
+  puts total 
+  return total
 end
 
 

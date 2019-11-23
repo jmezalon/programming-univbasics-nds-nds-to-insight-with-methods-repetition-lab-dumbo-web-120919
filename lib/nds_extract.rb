66096b026b7555fd0cfd543643590483b7b9b70d
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-23 11:37:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,7 +48,7 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   
-  pp source + " line 51"
+  p source + " line 51"
 end
 
 

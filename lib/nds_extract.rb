1fbfcdd9994b80fdeac3e7f53ca65ad27e6d597c
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-23 12:49:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def total_gross(source)
   index = 0 
   while index < source.length do
     
-    total += directors_totals(source)
+    total += directors_totals(source)[:worldwide_gross]
     
     index += 1
   end 

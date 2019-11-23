4192f30f9899ed05980962ae1d62bdd4f591308f
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-23 13:18:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,7 +50,7 @@ def total_gross(source)
   total = 0
   index = 0 
   while index < source.length do
-    key = list_of_directors(source[index])
+    key = list_of_directors(source)
     puts key
     index += 1
   end 
